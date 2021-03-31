10b35cac0e4c9862b9aebcac674f0a3409ae6293
nathanpeter636 <nathanpeter636@gmail.com>
2021-03-31 14:46:03 +1300

    added header comp and links, shop page contact and scrolltotop for routing

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import Homepage from './pages/Homepage'
 
 import ShopPage from './pages/shop/ShopPage'
 
+import Header from './components/header/Header'
+
 import './App.css'
+import ScrollToTop from './components/ScrollToTop';
 
 
 
 function App() {
   return (
     <>
+
+    <Header/>
+    <ScrollToTop/>
     <Switch>
+
     <Route exact path='/' component={Homepage} />
     <Route path='/shop' component={ShopPage} />
     </Switch>
---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        Logo
+      </Link>
+
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
